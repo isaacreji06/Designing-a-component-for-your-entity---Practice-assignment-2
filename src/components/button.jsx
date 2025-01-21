303ce0f8419760bcd00b46a31a0b03e05088f7e9
Isaac Reji <isaac.reji@kalvium.community>
2025-01-21 09:59:23 +0530

    project completed

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,12 @@
 // write button card here
+import React from 'react'
+
+function Button() {
+  return (
+    <div>
+        <button style={{background:'#2563eb',padding:'0.5rem',borderRadius:'0.5rem'}}>View Product</button>
+    </div>
+  )
+}
+
+export default Button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,25 @@
 // write product card here
+import React from 'react'
+import Button from './button'
+import photo from '../assets/circketBat.jpg'
+
+function Productcard() {
+    const product={
+        productName:"Cricket Bat and Ball",
+        price:100,
+    }
+  return (
+    <div style={{width:150,background:'white'}}>
+        <img src={photo} style={{width:150}}/>
+        <div style={{color:'black'}}>
+        {product.productName}
+        </div>
+        <div style={{color:'black'}}>
+        {product.price}$
+        </div>
+        <Button />
+    </div>
+  )
+}
+
+export default Productcard
